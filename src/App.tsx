aefba6656d6d19cd1942a811783d267d8f4a1276
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-09 16:13:35 +0700

    feat: Finishing for beta version

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export const App: React.FC = () => {
    const images = [
       "https://d15shllkswkct0.cloudfront.net/wp-content/blogs.dir/1/files/2013/10/chrome-hacked-story-300x300.jpg",
       "https://i.pinimg.com/736x/2d/70/1f/2d701f60a5697686370cb3a81ea5ca52.jpg",
-      "https://i.pinimg.com/474x/09/65/b9/0965b93f250920c1ee8a9a2b0ba0c291.jpg",
+      "https://www.clipartmax.com/png/small/180-1809318_picture-300-x-300-pixel.png",
    ];
 
    return (
