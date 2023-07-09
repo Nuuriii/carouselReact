cdd17b1b640fabe2a3421fd8cc058446e5bd70b6
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-09 14:17:34 +0700

    feat: add ImageSlider Component

--- src/Components/ImageSlider.tsx
+++ src/Components/ImageSlider.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { useState } from "react";
 
-export const ImageSlider = () => {
-   return <div>ImageSlider</div>;
+interface ImageSliderProps {
+   images: string[];
+}
+
+export const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
+   const [currentSlide, setCurrentSlide] = useState(0);
+
+   const goToPreviousSlide = () => {
+      setCurrentSlide((prevSlide) =>
+         prevSlide === 0 ? images.length - 1 : prevSlide - 1
+      );
+   };
+
+   const goToNextSlide = () => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % images.length);
+   };
+
+   return (
+      <div className='slider'>
+         <img src={images[currentSlide]} alt={`Slide ${currentSlide + 1}`} />
+         <button onClick={goToPreviousSlide}>Previous</button>
+         <button onClick={goToNextSlide}>Next</button>
+      </div>
+   );
 };
